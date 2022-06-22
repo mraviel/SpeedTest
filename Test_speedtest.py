1ddc175c0576a3332ec25b59e75527d831965dad
mraviel <mraviel@outlook.com>
2022-06-23 00:40:18 +0300

    Test completed

--- Test_speedtest.py
+++ Test_speedtest.py
@@ -1,8 +1,7 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from os import path
-from time import sleep
+import csv
 
 from Selenium_Classes.HomePage import HomePage
 
@@ -15,26 +14,35 @@ service_chrome = Service(chrome_driver_path)
 
 driver = webdriver.Chrome(service=service_chrome)
 
-#driver.set_page_load_timeout(10)
-
+print("Loading Page...")
 driver.get("https://www.speedtest.net/")
-
-
-driver.execute_script("window.stop();")
 
 driver.maximize_window()
 driver.implicitly_wait(10)
 
 home_page = HomePage(driver)
+
+# Click on go
 home_page.click_go_button()
 
-print(home_page.ping())
+ping = home_page.ping()
+print(f"Ping: {ping}")
 
+print("Computing download and upload...")
 while True:
     if "opacity: 0;" in home_page.speed_handle_opacity_style():
-        print(home_page.download_Mbps())
-        print(home_page.upload_Mbps())
+        download_mbps = home_page.download_Mbps()
+        upload_mbps = home_page.upload_Mbps()
+        print(f"Download Mbps: {download_mbps} \nUpload Mbps: {upload_mbps}")
         break
 
+# Add the values to the csv file
+with open('Data/data.csv', mode='w') as data:
+    writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+    writer.writerow(['Download', download_mbps])
+    writer.writerow(['Upload', upload_mbps])
+    writer.writerow(['Ping', ping])
+
+# Close the driver
 driver.close()
